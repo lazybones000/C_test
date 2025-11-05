33cf285c96e5ec0db64964d18cfba4a0720031e2
梁竣幃 <junweiliang26@gmail.com>
2025-11-06 03:02:33 +0800

    Refactor student score calculation and statistics
    01353008
    1.使用sum、scrore、average等有意變數名稱之假設。
    2.在使用輸入資訊前，給予友善提示文字，如:「請輸入學生人數」。
    3.將程式優化，減少bug，多新增不合理輸入之判斷，如：「輸入人數必須大於0、輸入成績必須介在0~100之間」。
    4.新增進階功能：找尋最高分及最低分、中位數，讓使用者更容易掌握分數分布狀況。

--- sum.c
+++ sum.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 
 int main() {
-    int n,i,s;
-    float x=0,y=0;
+    int quantity;
+    float score,average=0,sum=0,statistics[100];
 
-    scanf("%d",&n);
+	printf("請輸入學生人數:");
+    scanf("%d",&quantity);
 
-    for(i=1;i<=n;i++) {
-        scanf("%d",&s);
-        y=y+s;
+    if(quantity<=0) {
+        while (quantity > 0) {
+        printf("輸入人數必須大於0:");
+        scanf("%d", &quantity);
+        }
+	}
+	printf("請依序輸入各學生成績:\n");
+    for(int i=1;i<=quantity;i++) {
+		printf("第%d位:", i);
+        scanf("%f",&score);
+        while(score < 0 || score>100) {
+            printf("成績輸入錯誤,請重新輸入:");
+            scanf("%f",&score);
+		}
+		sum += score;
+		statistics[i - 1] = score;
     }
-
-    x=y/n;
-
-    printf("總成績=%.0f,平均=%.2f\n",y,x);
-
-    if(x<60)
-        printf("不及格");
-    else 
-        printf("及格");
+	//由小到大排序
+    for (int b = 0; b < quantity - 1; b++) {
+        for (int c = b + 1; c < quantity; c++) {
+            if (statistics[b] > statistics[c]) {
+                float temp = statistics[b];
+                statistics[b] = statistics[c];
+                statistics[c] = temp;
+            }
+        }
+    }
+	average = sum / quantity;
+    printf("\n成績統計結果：\n");
+    printf("總成績=%.2f,平均=%.2f\n",sum,average);
+	printf("最高分=%.2f,最低分=%.2f\n", statistics[quantity-1], statistics[0]);
+    if((quantity-1)%2==0)
+        printf("中位數=%.2f\n", statistics[(quantity-1)/2]);
+    else
+        printf("中位數=%.2f\n", (statistics[quantity / 2 - 1] + statistics[quantity / 2]) / 2);
+    printf("平均結果判定：%s\n", (average < 60) ? "不及格，要加油!" : "有及格，歡呼~");
 
     return 0;
 }
