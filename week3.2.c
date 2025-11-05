63bbda887023d2227aa615edb17832fda2baa51c
rain01353027 <01353027@mail.ntou.edu.tw>
2025-11-06 02:59:51 +0800

    Update week3.2.c
    1.將代數名修改至較易辨認
    2.優化34~37行並刪除不必要的代數x
    3.於第6行加入執行時出現的指引

--- week3.2.c
+++ week3.2.c
@@ -1,42 +1,52 @@
 #include <stdio.h>
 int main(){
-    float n[100],x;
+    float num[100];
     int a;
-    char ch[5];
-    printf("please input numbers:");
+    char ch[5], end='g';
+    printf("輸入數值尋找最大值、最小值、中位數；輸入g完成輸入\nplease input numbers:\n");
     for(a=0;a<100;a++){
-        if(scanf("%f",&n[a]))
+        if(scanf("%f",&num[a])){
             continue;
+        }
         else{
             scanf("%s",&ch);
-            if(ch[0]!='g'){
+            if(ch[0]!=end){
                 while(1){
                     printf("number非數值,請重新輸入:");
-                    if(scanf("%f ",&n[a]))
+                    if(scanf("%f ",&num[a]))
                         break;
                     else{
                         scanf("%s",&ch);
-                        if(ch[0]=='g')
+                        if(ch[0]== end){
                             break;
-                        continue;}
+                        }
+                        continue;
+                    }
                 }
             }
-            if(ch[0]=='g')
-                break;}
+            if(ch[0]== end){
+                break;
+            }
+        }
     } 
     for(int b=0;b<a-1;b++){
         for(int c=b+1;c<a;c++){
-            if(n[b]>n[c]){
-                x=n[b];
-                n[b]=n[c];
-                n[c]=x;}
+            if(num[b]>num[c]){
+                num[b]=num[b]+num[c];
+                num[c]=num[b]-num[c];
+                num[b]=num[b]-num[c];
+            }
         }
     }
-    if((a-1)%2==0)
-        printf("中位數=%f\n",n[(a-1)/2]);
-    if((a-1)%2==1)
-        printf("中位數=%f\n",(n[(a-2)/2]+n[a/2])/2);
-    printf("最小值=%f\n",n[0]);
-    printf("最大值=%f",n[a-1]);
+    //若數據量為奇數
+    if((a-1)%2==0){
+        printf("中位數=%f\n",num[(a-1)/2]);
+    }
+    //若數據量為偶數
+    else{
+        printf("中位數=%f\n",(num[(a-2)/2]+num[a/2])/2);
+    }
+    printf("最小值=%f\n",num[0]);
+    printf("最大值=%f",num[a-1]);
     return 0;
 }
