3dd327e9f237ca385b0b3377f73e8fae935a2e97
wyq1906 <01353029@mail.ntou.edu.tw>
2025-11-06 13:06:15 +0800

    檢測輸入數字是否為質數

--- 01353014.c
+++ 01353014.c
@@ -1,33 +1,60 @@
 #include <stdio.h>
-int i,isPrime;
-float n;
+#include <math.h>//平方根計算
 
 int main(){
+    //從全域變數換成區域變數
+    int is_prime = 1;
+    double input_num;
 
-    scanf("%f",&n);
+    //新增輸入提示
+    printf("請輸入一個整數: ");
 
-    if(n - (int)n != 0){
-        printf("請輸入整數");
-        return 0;
+    //檢查是否為數字
+    if (scanf("%lf", &input_num) != 1) {
+        printf("輸入錯誤！請輸入有效的數字\n");
+        return 1;
     }
 
-    if(n <= 1){
-        printf("請輸入大於1的整數");
-        return 0;
+    //檢查是否為整數
+    if (input_num != (int)input_num) {
+        printf("請輸入整數\n");
+        return 1;
     }
 
-    for(i=2;i <= n-1;i++){
-        if((int)n % i == 0){
-            printf("%.0f不是質數\n除1和%.0f外的因數有:%d",n,n,i);
-            isPrime = 0;
-            break;
-        }
-        else
-            isPrime = 1;
-    } 
+    //轉換成整數形式
+    int num = (int)input_num;
+
+    //檢查是否大於1
+    if (num <= 1) {
+        printf("請輸入大於1的整數\n");
+        return 1;
+    }
+
+    // 處理特例
+    if (num == 2) {
+        printf("%d是質數\n", num);
+        return 0;
+    }
     
-    if(isPrime)   
-        printf("%.0f是質數\n",n);
+    if (num % 2 == 0) {//檢查是否為偶數
+        printf("%d不是質數\n除1和%d外的因數有: 2", num, num);
+        is_prime = 0;
+    } 
+    else {// 只需檢查到平方根即可
+        int limit = (int)sqrt(num) + 1;
+        
+        for (int i = 3; i <= limit; i += 2) {
+            if (num % i == 0) {
+                printf("%d不是質數\n除1和%d外的因數有: %d", num, num, i);
+                is_prime = 0;
+                break;
+            }
+        }
+    }
 
+    //輸出結果
+    if (is_prime) 
+        printf("%d是質數\n", num);
+    
     return 0;       
 }
