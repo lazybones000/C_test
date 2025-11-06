c61a8113eca7b1c3241cf436bafdb9a7ceab0f6a
Wayne Tsai <131647592+Wayne1234567892427875@users.noreply.github.com>
2025-11-06 15:50:08 +0800

    Add files via upload

---
+++ 111/README.md
@@ -0,0 +1,4 @@
+# 111深廣 作業用
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/EJQG/pen/ZEMJzVX](https://codepen.io/EJQG/pen/ZEMJzVX).
+
---
+++ 111/src/script.js
@@ -0,0 +1,8 @@
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count++;
+      counterElement.textContent = count;
+    }
