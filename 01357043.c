113718a4b5571ab19ae2296fc2e4fb943d99f8da
ASUS-ZENBOOK\RD <oscaryan950405@gmail.com>
2025-11-06 16:21:56 +0800

    01357043.c幫你寫作業

--- 01357043.c
+++ 01357043.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
-int main()
-{
-    printf("作業是這樣交的嗎?");
-}//todo
+#include <stdlib.h>
+
+#define MAX 1000
+
+int main() {
+    int queue[MAX];
+    int size;
+    int front = 0, rear = 0;
+    int cmd, x;
+
+    scanf("%d", &size); 
+
+    while (1) {
+        scanf("%d", &cmd);
+        if (cmd == -1)
+            break;
+
+        if (cmd == 1) { 
+            scanf("%d", &x);
+            if ((rear + 1) % size == front) {
+                printf("FULL\n");
+            } else {
+                queue[rear] = x;
+                rear = (rear + 1) % size;
+            }
+        } else if (cmd == 0) {
+            if (front == rear) {
+                printf("EMPTY\n");
+            } else {
+                printf("%d\n", queue[front]);
+                front = (front + 1) % size;
+            }
+        }
+    }
+
+    return 0;
+}
